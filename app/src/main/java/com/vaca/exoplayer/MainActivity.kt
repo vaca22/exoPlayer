59c724df43bc85d2c29d0508fbea995bf10772dc
liguanghao <liguanghao@viatomtech.com>
2022-12-21 10:58:49 +0800

    dfg32564

--- app/src/main/java/com/vaca/exoplayer/MainActivity.kt
+++ app/src/main/java/com/vaca/exoplayer/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         val player = ExoPlayer.Builder(this).build()
         val mediaItem: MediaItem = MediaItem.Builder()
             .setUri("http://stream.eudic.net/fr_rfimonde.webm?agent=%2feusoft_ting_fr_android%2f10.4.3%2fc36080d41a2b4161%2f%2f%2f&token=QYN+eyJ0b2tlbiI6IiIsInVzZXJpZCI6IiIsInVybHNpZ24iOiJZY0RuVzlOT0FGaGFHckdjZzFUS3MwaXY2eDg9IiwidCI6IkFCSU1UWTVNVFF3T1RFMk5nPT0ifQ%3d%3d&stamp=638072140553023803")
-            .setMimeType("audio/webm")
             .build()
 
         player.setMediaItem(mediaItem)
