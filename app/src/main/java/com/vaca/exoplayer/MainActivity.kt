1f6bf2f4afadaab4040f2ca3d9be11d45aa78dd1
liguanghao <liguanghao@viatomtech.com>
2022-12-21 11:11:10 +0800

    dfg3256466

--- app/src/main/java/com/vaca/exoplayer/MainActivity.kt
+++ app/src/main/java/com/vaca/exoplayer/MainActivity.kt
@@ -7,13 +7,15 @@ import com.google.android.exoplayer2.MediaItem
 
 
 class MainActivity : AppCompatActivity() {
+    val x2="http://stream.eudic.net/fr_franceculture.mp3?agent=%2feusoft_ting_fr_android%2f10.4.3%2fc36080d41a2b4161%2f%2f%2f&token=QYN+eyJ0b2tlbiI6IiIsInVzZXJpZCI6IiIsInVybHNpZ24iOiJYUGI1aW5mNW5ueUpJdTMxS05kZ0pkdUZXV289IiwidCI6IkFCSU1UWTVNVFF4TWpVeU5RPT0ifQ%3d%3d&stamp=638072174135316410"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val player = ExoPlayer.Builder(this).build()
         val mediaItem: MediaItem = MediaItem.Builder()
-            .setUri("http://stream.eudic.net/fr_rfimonde.webm?agent=%2feusoft_ting_fr_android%2f10.4.3%2fc36080d41a2b4161%2f%2f%2f&token=QYN+eyJ0b2tlbiI6IiIsInVzZXJpZCI6IiIsInVybHNpZ24iOiJZY0RuVzlOT0FGaGFHckdjZzFUS3MwaXY2eDg9IiwidCI6IkFCSU1UWTVNVFF3T1RFMk5nPT0ifQ%3d%3d&stamp=638072140553023803")
+            .setUri(x2)
+            .setMimeType("audio/mp3")
             .build()
 
         player.setMediaItem(mediaItem)
